9ce96736fd9b5d10d8e8e9492fa00113808a2eda
Kabongwe-Manelisi <118818827+Kabongwe-Manelisi@users.noreply.github.com>
2026-08-17 13:21:21 +0000

    script.js update

--- static/script.js
+++ static/script.js
@@ -126,3 +126,17 @@ window.smoothScroll = function(target) {
     
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
+//
+// app.html catagory functions
+//
+// catagoryOne ()
+function catagoryOne() {
+    document.getElementById('productContainer').innerHTML = `
+    {% for catagoryOne in catagory %}
+    <h1>
+        {{ catagoryOne }}
+    </h1>
+    {% endfor %}
+    `
+}
